fa0ab96a6ad8b4e101d44cbfbbd90e60be842f99
sum20171181 <2017118101@stu.hstc.edu.cn>
2019-09-19 09:53:04 +0800

    my application

---
+++ my application/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+}
