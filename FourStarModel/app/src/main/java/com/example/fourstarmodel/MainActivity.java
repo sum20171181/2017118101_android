974a54f3a93edb01e60a6cd46ad315c79c253e07
sum20171181 <2017118101@stu.hstc.edu.cn>
2019-10-04 23:28:28 +0800

    启动模式singleInstance

---
+++ FourStarModel/app/src/main/java/com/example/fourstarmodel/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.fourstarmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+        setContentView(R.layout.activity_main);
+        Button Hello1=(Button) findViewById(R.id.Hello1);
+        Button Hello2=(Button) findViewById(R.id.Hello2);
+        Button Hello3=(Button) findViewById(R.id.Hello3);
+        Hello2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        Hello3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,ThridActivity.class);
+                startActivity(intent);
+            }
+        });
+        Hello1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+    }
+
---
+++ FourStarModel/app/src/main/java/com/example/fourstarmodel/ThridActivity.java
@@ -0,0 +1,72 @@
+package com.example.fourstarmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ThridActivity extends AppCompatActivity {
+    public static final String TAG="ThridActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"OnCreaate");
+        setContentView(R.layout.activity_thrid);
+        Button Hello1=(Button) findViewById(R.id.Hello1);
+        Button Hello2=(Button) findViewById(R.id.Hello2);
+        Button Hello3=(Button) findViewById(R.id.Hello3);
+        Hello1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(ThridActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Hello2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(ThridActivity.this,MainActivity.class);
+            }
+        });
+        Hello3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(ThridActivity.this,ThridActivity.class);
+            }
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+    }
+
